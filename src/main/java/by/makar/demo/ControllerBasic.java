fd8c314a5f5a28dc8a6bb3baa745253ac3722f87
MqArrr <makar125170@gmail.com>
2022-11-07 21:04:32 +0300

    works

--- src/main/java/by/makar/demo/ControllerBasic.java
+++ src/main/java/by/makar/demo/ControllerBasic.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/hello")
 public class ControllerBasic {
-    @GetMapping("/hello")
+    @GetMapping
     public String hello(){
         return "hello";
     }
